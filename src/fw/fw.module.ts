1cf0a614388b4a5ec9c885dbda93c4079e165658
Andrii Tkach <andrii.tkach@invensus.com>
2017-12-28 12:07:42 +0200

    Added responsive views and services

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -4,6 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+
+import { FrameworkConfigService } from './services/framework-config.service';
+import { ScreenService } from './services/screen.service';
+
+import { ScreenLarge } from './directives/screen-large.directive';
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
 
 @NgModule({
   imports: [
@@ -12,7 +20,15 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
   declarations: [
     FrameworkBodyComponent,
     ContentComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    TopBarComponent,
+    StatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
+  ],
+  providers: [
+    FrameworkConfigService,
+    ScreenService
   ],
   exports: [
     FrameworkBodyComponent
